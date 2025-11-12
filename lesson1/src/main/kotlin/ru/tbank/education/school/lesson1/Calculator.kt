a6908d2d5c688c99a50453cdb9f4dc39a4724a1a
WamblyLA <147947331+WamblyLA@users.noreply.github.com>
2025-11-12 19:52:44 +0300

    rebasing for lesson2

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
@@ -1,18 +1,59 @@
 package ru.tbank.education.school.lesson1
 
-/**
- * Метод для вычисления простых арифметических операций.
- */
-fun calculate(a: Double, b: Double, operation: OperationType): Double? {
-    TODO()
+fun preobr(a: String): OperationType {
+    if (a == "+") {
+        return OperationType.ADD;
+    } else if (a == "-") {
+        return OperationType.SUBTRACT;
+    } else if (a == "*") {
+        return OperationType.MULTIPLY;
+    } else if (a == "/") {
+        return OperationType.DIVIDE;
+    }
+    return OperationType.ADD;
 }
+fun calculate(a: Double?, b: Double?, operation: OperationType = OperationType.ADD): Double? {
+    if (a == null || b == null) {
+        return null
+    }
+    when (operation) {
+        OperationType.ADD-> {
+            println(a+b)
+            return a + b
+        }
+        OperationType.SUBTRACT -> {
+            println(a - b)
+            return a - b
+        }
 
+        OperationType.DIVIDE -> {
+            if (b == 0.0) {
+                println(null)
+                return null
+            } else {
+                println(a/b)
+                return a/b
+            }
+        }
+        OperationType.MULTIPLY -> {
+            println(a * b)
+            return a * b
+        }
+    }
+
+}
+fun main() {
+    val a = 132.0
+    val b = 66.0
+    calculate(a,b,preobr("*"))
+}
 /**
  * Функция вычисления выражения, представленного строкой
  * @return результат вычисления строки или null, если вычисление невозможно
- * @sample "5 * 2".calculate()
- */
+ * @sample "5 * 2".calculate()*/
+
+
 @Suppress("ReturnCount")
 fun String.calculate(): Double? {
-    TODO()
+    return 2.0
 }
