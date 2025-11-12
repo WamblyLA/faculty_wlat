cd3bf7d0479824fc496ca62caddada6522a24191
WamblyLA <147947331+WamblyLA@users.noreply.github.com>
2025-11-12 22:17:32 +0300

    Added reverse polish notation for string math

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/OperationType.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/OperationType.kt
@@ -3,6 +3,12 @@ package ru.tbank.education.school.lesson1
 /**
  * Список операций.
  */
-enum class OperationType {
-    ADD, SUBTRACT, MULTIPLY, DIVIDE
+enum class OperationType(val possible: List<String>) {
+    ADD(listOf("добавить", "add", "+")),
+    DIVIDE(listOf("разделить", "divide", "/")),
+    MULTIPLY(listOf("multiply", "*", "умножить")),
+    SUBTRACT(listOf("subtract", "-", "вычесть")),
+    SIN(listOf("sin")),
+    COS(listOf("cos")),
+    SQRT(listOf("sqrt")),
 }
