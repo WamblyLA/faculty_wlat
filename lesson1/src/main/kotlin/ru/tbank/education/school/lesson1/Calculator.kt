71ed2136ee52c2d9f0debb1f816835152268b298
WamblyLA <147947331+WamblyLA@users.noreply.github.com>
2025-11-12 22:17:32 +0300

    Added reverse polish notation for string math

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
@@ -1,18 +1,91 @@
 package ru.tbank.education.school.lesson1
-
-fun preobr(a: String): OperationType {
-    if (a == "+") {
-        return OperationType.ADD;
-    } else if (a == "-") {
-        return OperationType.SUBTRACT;
-    } else if (a == "*") {
-        return OperationType.MULTIPLY;
-    } else if (a == "/") {
-        return OperationType.DIVIDE;
+fun isExisting(str: String): Boolean {
+    for (k in OperationType.entries) {
+        if (k.possible.any {it.lowercase() == (str.lowercase())}) {
+            return true;
+        }
     }
-    return OperationType.ADD;
+    return false;
 }
-fun calculate(a: Double?, b: Double?, operation: OperationType = OperationType.ADD): Double? {
+fun preobrazovatel(str: String): String {
+    when (str.lowercase()) {
+        in listOf("add", "добавить") -> return "+"
+        in listOf("subtract", "вычесть") -> return "-"
+        in listOf("multiply", "умножить") -> return "*"
+        in listOf("sin") -> return "sin"
+        in listOf("cos") -> return "cos"
+        in listOf("sqrt") -> return "sqrt"
+        in listOf("divide", "разделить") -> return "/"
+    }
+    return str.lowercase();
+}
+fun priority(str: String): Int {
+    when (str) {
+        in listOf("+", "-") -> return 1
+        in listOf("*", "/") -> return 2
+    }
+    return 0;
+}
+fun toOPZ(str: String): ArrayList<String> {
+    var returning = ArrayList<String>();
+    var stack = ArrayDeque<String>();
+    var i = 0
+    while (i < str.length) {
+        val char = str[i];
+        if (char.isWhitespace()) {
+            i++
+            continue;
+        }
+        if (char.isDigit() || char == '.') {
+            var newNum = ""
+            while (i < str.length && (str[i].isDigit() || str[i] == '.')) {
+                newNum += str[i];
+                i++
+            }
+            returning.add(newNum);
+        } else if (char.isLetter()) {
+            var newOper = ""
+            while (i < str.length && str[i].isLetter()) {
+                newOper += str[i];
+                i++
+            }
+            if (isExisting(newOper)) {
+                val preobr = preobrazovatel(newOper);
+                while (!stack.isEmpty() && stack.last() != "(" && priority(stack.last()) >= priority(preobr)) {
+                    returning.add(stack.removeLast());
+                }
+                stack.addLast(preobr);
+            } else {
+                return ArrayList<String>();
+            }
+        } else if (char == '(') {
+            stack.addLast("(")
+            i++;
+        } else if (char == ')') {
+            while (!stack.isEmpty() && stack.last() != "(") {
+                returning.add(stack.removeLast());
+            }
+            if (!stack.isEmpty() && stack.last() == "(") {
+                stack.removeLast();
+            } else {
+                return ArrayList<String>();
+            }
+            i++;
+        } else {
+            val procheck = char.toString();
+            while (!stack.isEmpty() && stack.last() != "(" && priority(stack.last()) >= priority(procheck)) {
+                returning.add(stack.removeLast());
+            }
+            stack.addLast(procheck);
+            i++;
+        }
+    }
+    while (!stack.isEmpty()) {
+        returning.add(stack.removeLast());
+    }
+    return returning;
+}
+/*fun calculate(a: Double?, b: Double?, operation: OperationType = OperationType.ADD): Double? {
     if (a == null || b == null) {
         return null
     }
@@ -40,12 +113,17 @@ fun calculate(a: Double?, b: Double?, operation: OperationType = OperationType.A
             return a * b
         }
     }
-
-}
+    return 0.0
+}*/
 fun main() {
-    val a = 132.0
-    val b = 66.0
-    calculate(a,b,preobr("*"))
+    val arr = toOPZ("((1add2)* 3) вычесть1")
+    if (arr.isEmpty()) {
+        println("Кажется, в выражении что-то не так...")
+    }
+    for (k in arr){
+        print(k)
+        print(" ")
+    }
 }
 /**
  * Функция вычисления выражения, представленного строкой
@@ -53,7 +131,8 @@ fun main() {
  * @sample "5 * 2".calculate()*/
 
 
-@Suppress("ReturnCount")
+/*@Suppress("ReturnCount")
 fun String.calculate(): Double? {
     return 2.0
 }
+*/
