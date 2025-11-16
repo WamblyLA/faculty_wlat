ac864305f54bbad53b550a8cd757d9dd305319d6
WamblyLA <147947331+WamblyLA@users.noreply.github.com>
2025-11-16 22:08:43 +0300

    rebase

--- lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/homework/University.kt
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/homework/University.kt
@@ -1,24 +1,4 @@
 package ru.tbank.education.school.lesson2.homework
 
-import ru.tbank.education.school.lesson2.homework.people.Person
-import ru.tbank.education.school.lesson2.homework.people.Roles
-import ru.tbank.education.school.lesson2.homework.study.Exam
 class University {
-    private val people = mutableListOf<Person>()
-    private val exams = mutableListOf<Exam>()
-    fun addPerson(person: Person) {
-        people.add(person)
-    }
-    fun removePerson(person: Person) {
-        people.remove(person)
-    }
-    fun addExam(exam: Exam) {
-        exams.add(exam)
-    }
-    fun removeExam(exam: Exam) {
-        exams.remove(exam)
-    }
-    fun getExams(): List<Exam> = exams
-    fun getStudents(): List<Person> = people.filter {it.personRole == Roles.STUDENT }
-    fun getTeachres(): List<Person> = people.filter {it.personRole == Roles.TEACHER}
 }
