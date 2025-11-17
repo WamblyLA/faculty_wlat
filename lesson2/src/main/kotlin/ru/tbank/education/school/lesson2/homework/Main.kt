5010dd239b91b8c513a769e9ffc70ef34dd94b3d
WamblyLA <147947331+WamblyLA@users.noreply.github.com>
2025-11-17 22:24:44 +0300

    Done hw2

---
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/homework/Main.kt
@@ -0,0 +1,35 @@
+package ru.tbank.education.school.lesson2.homework
+
+import ru.tbank.education.school.lesson2.homework.people.Student
+import ru.tbank.education.school.lesson2.homework.people.Teacher
+import java.time.LocalDate
+
+fun main() {
+    val university = University()
+    val teacher = Teacher(
+        id = generateRandomString(10),
+        name = "Ivan Ivanov",
+        birthDate = LocalDate.of(1990, 1, 1),
+        cadefr = "FCS",
+        email = "tbank@kbant.com",
+        position = "Head teacher"
+    )
+    val student = Student(
+        id = generateRandomString(10),
+        name = "Petr Petrov",
+        birthDate = LocalDate.of(2005, 1, 1),
+        email = "tbank@tankb.com",
+        faculty = "Economics and Data Analysis"
+    )
+    university.addPerson(student)
+    university.addPerson(teacher)
+    val exam = teacher.createExam(university, "Module 1 Exam", LocalDate.of(2025,11,20), desc="This is our first exam")
+    teacher.addQuestions(exam, listOf(mapOf("first" to "1+1", "second" to "2+2"), mapOf("first" to "2+3"), mapOf("second" to "3+4")))
+    println("Вопросов теперь ${exam.getAmountOfQuestions()}")
+    student.takePart(exam);
+    student.finishExam(exam);
+    teacher.rateStudent(exam, student, 15);
+    println("Студент оценен в ${student.getGrade(exam)}")
+    teacher.deleteExam(university,exam);
+    println("После удаления экзаменов ${university.getExams().size}")
+}
