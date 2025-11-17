193308a8721135f938db38349c76aa8ffcfe830c
WamblyLA <147947331+WamblyLA@users.noreply.github.com>
2025-11-17 22:24:44 +0300

    rebase

--- lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/homework/study/Exam.kt
+++ lesson2/src/main/kotlin/ru/tbank/education/school/lesson2/homework/study/Exam.kt
@@ -1,20 +1,30 @@
 package ru.tbank.education.school.lesson2.homework.study
 
 import ru.tbank.education.school.lesson2.homework.people.Student
+import ru.tbank.education.school.lesson2.homework.people.Teacher
 import java.time.LocalDate
 
 class Exam (
     val id: String,
+    val name: String,
     conductDate: LocalDate,
+    val teacher: Teacher,
     var description: String,
-    var grades: MutableMap<Student, Int>,
-    private var questions: MutableList<Question>,
+    var grades: MutableMap<Student, Int> = mutableMapOf(),
+    private var questions: MutableList<Question> = mutableListOf(),
 ) {
     fun getGrade(student: Student): Int {
         return grades[student] ?: -1
     }
+    fun getAmountOfQuestions(): Int {
+        val sizik: Int = questions.size
+        return sizik
+    }
     fun addQuestion(question: Question) {
         questions.add(question)
+    }
+    fun removeQuestion(question: Question) {
+        questions.remove(question)
     }
     internal fun join(student: Student) {
         grades[student] = grades.getOrDefault(student, -1)
