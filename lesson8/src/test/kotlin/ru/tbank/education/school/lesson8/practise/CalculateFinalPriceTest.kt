8a66bfffb99aa2a15fc254e1d928183fcb6e0d96
WamblyLA <147947331+WamblyLA@users.noreply.github.com>
2026-01-15 11:00:11 +0300

    rebase commit

--- lesson8/src/test/kotlin/ru/tbank/education/school/lesson8/practise/CalculateFinalPriceTest.kt
+++ lesson8/src/test/kotlin/ru/tbank/education/school/lesson8/practise/CalculateFinalPriceTest.kt
@@ -1,5 +1,57 @@
 package ru.tbank.education.school.lesson8.practise
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertThrows
+
+
+class CalculateFinalPriceTest {
+    //  1.1
+    @Test
+    fun `default case`() {
+        val result = 1080.0;
+        assertEquals(result, calculateFinalPrice(1000.0, 10, 20))
+    }
+    //  1.2
+    @Test
+    fun `no discount`() {
+        val result = 1200.0;
+        assertEquals(result, calculateFinalPrice(1000.0, 0, 20))
+    }
+    //  1.3
+    @Test
+    fun `no tax`() {
+        val result = 900.0;
+        assertEquals(result, calculateFinalPrice(1000.0, 10, 0))
+    }
+    //  1.4
+    @Test
+    fun `no params`() {
+        val result = 1000.0;
+        assertEquals(result, calculateFinalPrice(1000.0, 0, 0))
+    }
+    //  2.1
+    @Test
+    fun `negative price`() {
+        assertThrows(IllegalArgumentException::class.java) {
+            calculateFinalPrice(-1000.0, 10, 20);
+        }
+    }
+    //  2.2
+    @Test
+    fun `illegal discount`() {
+        assertThrows(IllegalArgumentException::class.java) {
+            calculateFinalPrice(1000.0, 200, 0);
+        }
+    }
+    //  2.3
+    @Test
+    fun `illegal tax`() {
+        assertThrows(IllegalArgumentException::class.java) {
+            calculateFinalPrice(1000.0, 20, -3);
+        }
+    }
+}
 /**
  *
  * Сценарии для тестирования:
